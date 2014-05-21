c2c9e7de1e937c7adffe681ad653fbee7e86c2eb
Wayne Berry <wayneashleyberry@gmail.com>
2014-05-22 00:44:55 +0200

    Revert "move linter into libs"
    This reverts commit 5473e51534d8e58b87a095da083c9c2a3e80dfc4.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 'use strict';
 
 var q = require('q');
+var shell = require('shelljs/global');
 var async = require('async');
 
-var linter = require('./lib/linter');
+var cache = require('./lib/cache');
 var processPatterns = require('./lib/process');
+
+var stdout = false;
+
+var linter = function (file, cb) {
+  cache.has(file, function(err, isCached, hash) {
+    if (err) return cb(err);
+
+    if (isCached) {
+      if (stdout) {
+        cache.get(hash, function (err, cached) {
+          process.stdout.write(cached.contents);
+        });
+      }
+      return cb();
+    }
+
+    exec('php -l '+file, {silent: ! stdout}, function (code, output) {
+      var err = (code === 0) ? null : output.trim();
+
+      if (err) return cb(err);
+
+      cache.put(hash, output, cb);
+    });
+  });
+};
 
 module.exports = function (patterns, options) {
   var deferred = q.defer();
@@ -13,11 +39,9 @@ module.exports = function (patterns, options) {
   options = options || {};
 
   var limit = options.limit ? options.limit : 10;
-  var stdout = options.stdout ? options.stdout : false;
+  stdout = options.stdout ? options.stdout : false;
 
-  async.eachLimit(result, limit, function (file, cb) {
-    linter(file, {stdout: stdout}, cb);
-  }, function (err) {
+  async.eachLimit(result, limit, linter, function (err) {
     if (err) return deferred.reject(new Error(err.trim()));
 
     deferred.resolve(result.length + ' file(s) were linted.');
