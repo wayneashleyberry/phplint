e549dffb2674176578205525ab51e2cdae61f3d9
Wayne Berry <wayneashleyberry@gmail.com>
2014-05-21 23:19:01 +0200

    tests for php executable

--- test/test.js
+++ test/test.js
@@ -3,6 +3,15 @@
 var assert = require('assert');
 var phplint = require('../index');
 
+require('shelljs/global');
+
+it('should have php', function (cb) {
+  if (! which('php')) {
+    assert.fail(which('php'), '/usr/local/bin/php');
+  }
+  cb();
+});
+
 it('should call the fail method', function (cb) {
   var lint = phplint('test/*.php');
   lint.fail(function (err) {
