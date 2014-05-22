2428d0b2d0e4e8704a3aae6de91c1806663eea3b
Wayne Berry <wayneashleyberry@gmail.com>
2014-05-22 10:34:04 +0200

    moves linter into separate file

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
 'use strict';
 
 var q = require('q');
-var shell = require('shelljs/global');
 var async = require('async');
-
-var cache = require('./lib/cache');
+var linter = require('./lib/linter');
 var processPatterns = require('./lib/process');
-
-var stdout = false;
-
-var linter = function (file, cb) {
-  cache.has(file, function(err, isCached, hash) {
-    if (err) return cb(err);
-
-    if (isCached && stdout) {
-      return cache.get(hash, function (err, cached) {
-        process.stdout.write(cached.contents);
-        cb();
-      });
-    }
-
-    if (isCached && ! stdout) {
-      return cb();
-    }
-
-    exec('php -l '+file, {silent: ! stdout}, function (code, output) {
-      var err = (code === 0) ? null : output.trim();
-
-      if (err) return cb(err);
-
-      cache.put(hash, output, cb);
-    });
-  });
-};
 
 module.exports = function (patterns, options) {
   var deferred = q.defer();
@@ -41,9 +12,9 @@ module.exports = function (patterns, options) {
   options = options || {};
 
   var limit = options.limit ? options.limit : 10;
-  stdout = options.stdout ? options.stdout : false;
+  linter.stdout = options.stdout ? options.stdout : false;
 
-  async.eachLimit(result, limit, linter, function (err) {
+  async.eachLimit(result, limit, linter.lint.bind(linter), function (err) {
     if (err) return deferred.reject(new Error(err.trim()));
 
     deferred.resolve(result.length + ' file(s) were linted.');
