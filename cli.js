59e79aaedd550606a553be18e64b2bf036ee4058
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-16 14:05:15 +0200

    complete rewrite, focusing on the command line

---
+++ cli.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var globby = require('globby');
+var async = require('async');
+var exec = require('child_process').exec;
+var args = process.argv.slice(2);
+var limit = 10;
+
+if (args.length === 0) {
+  throw new Error('Invalid argument length');
+  process.exit(1);
+}
+
+exec('php -v', function(err, stdout, stderr) {
+  if (err) {
+    throw new Error(err);
+    process.exit(1);
+  }
+
+  console.log(stdout);
+});
+
+globby(args, function (err, paths) {
+  if (err) throw new Error(err);
+
+  async.eachLimit(paths, limit, function (item, callback) {
+    // run on each item
+    var child = exec('php -l '+item, {
+      cwd: process.cwd(),
+      env: process.env
+    }, function (err, stdout, stderr) {
+      // done on a single item
+      process.stdout.write(stdout);
+      process.stderr.write(stderr);
+      callback(err);
+    })
+  }, function (err) {
+    // all items are done
+    if (err) process.exit(1);
+  });
+});
---
+++ test/zef.php
@@ -0,0 +1,3 @@
+<?php
+
+ZEFSIDE
