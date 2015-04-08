66c5c8feac89b534ea2de39da60b037b62965243
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-04-08 13:04:27 +0200

    adds -v and --version flags

--- cli.js
+++ cli.js
@@ -8,6 +8,14 @@ if (args.length === 0) {
   throw new Error('Invalid argument length');
 }
 
+if (args[0] === '-v' || args[0] === '--version') {
+  var fs = require('fs');
+  var file = fs.readFileSync(__dirname + '/package.json', {encoding: 'utf8'});
+  var package = JSON.parse(file);
+  console.log(package.version);
+  return;
+}
+
 exec('php -v', function(err, stdout, stderr) {
   if (err) throw new Error(err);
 });
